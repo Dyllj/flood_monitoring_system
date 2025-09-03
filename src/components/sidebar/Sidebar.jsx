6f110eb7c88ff19de5491a74dd145675c48455bf
Dyll <wtautoan73@gmail.com>
2025-09-03 13:52:11 +0800

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import Home from "../sidebar_components/Home";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        <Home/>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/sidebar_components/Home.jsx
@@ -0,0 +1,6 @@
+function Home() {
+    return (
+        <a href="" className="Home">Home</a>
+    )
+}
+export default Home;
