0e70cd850b87ecc9a6268cb7388ec678027c9941
Dyll <wtautoan73@gmail.com>
2025-09-03 14:52:54 +0800

    header component update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Sidebar from './components/sidebar/sidebar'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Header />
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import './header.css'
+function Header() {
+    return (
+        <header className="Header">
+            <h1>Flood Monitoring System</h1>
+        </header>
+    )
+}
+export default Header;
