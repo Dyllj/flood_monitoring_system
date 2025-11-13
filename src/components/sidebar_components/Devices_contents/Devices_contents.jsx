9ca068095da06a387ff1a2a37af933319d0ff92f
Dyll <wtautoan73@gmail.com>
2025-11-13 13:34:19 +0800

    css

--- src/components/sidebar_components/Devices_contents/Devices_contents.jsx
+++ src/components/sidebar_components/Devices_contents/Devices_contents.jsx
@@ -303,7 +303,7 @@ const Devices_contents = ({ isAdmin }) => {
                       </linearGradient>
                     </defs>
                     <Area
-                      type="monotone"
+                      type="linear"
                       dataKey="value"
                       stroke={color}
                       fill={`url(#waterColor-${device.id})`}
